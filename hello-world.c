370e0ad2378f94e8057f1de0dafe975201852cdc
jawad219 <jawadbinmujib219@gmail.com>
2024-11-30 01:41:21 +0600

    Updating at local directory

--- hello-world.c
+++ hello-world.c
@@ -1,6 +1,4 @@
 #include<stdio.h> ---> is a preprocessor directive that tells the compiler to include the Standard Input/Output library (stdio.h) in your program.
-int  
-
 int --> It means main() will return an integer value to the operating system when the program finishes executing.
 main() --> This is the name of the function where the program starts executing.
 () --> In this case, it takes no parameters (empty parentheses).
