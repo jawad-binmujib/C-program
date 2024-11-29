b31892adbfcc05b96d54762b99eb5b21aa94502c
jawad219 <jawadbinmujib219@gmail.com>
2024-11-30 01:39:14 +0600

    Adding int main()

--- hello-world.c
+++ hello-world.c
@@ -1,2 +1,6 @@
 #include<stdio.h> ---> is a preprocessor directive that tells the compiler to include the Standard Input/Output library (stdio.h) in your program.
-int main() 
+int  
+
+int --> It means main() will return an integer value to the operating system when the program finishes executing.
+main() --> This is the name of the function where the program starts executing.
+() --> In this case, it takes no parameters (empty parentheses).
