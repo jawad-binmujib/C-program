ed2bc9019079ee2e8026abe6e8ad0f50f5c4d99a
jawad219 <jawadbinmujib219@gmail.com>
2024-11-30 11:33:42 +0600

    commenting the description

--- hello-world.c
+++ hello-world.c
@@ -1,10 +1,10 @@
-#include<stdio.h> ---> is a preprocessor directive that tells the compiler to include the Standard Input/Output library (stdio.h) in your program.
+/*#include<stdio.h> ---> is a preprocessor directive that tells the compiler to include the Standard Input/Output library (stdio.h) in your program.
 int --> It means main() will return an integer value to the operating system when the program finishes executing.
 main() --> This is the name of the function where the program starts executing.
 () --> In this case, it takes no parameters (empty parentheses).
 printf --> It's a function that used to display output on the screen
 This printf statement outputs the text "Hello World" to the screen.
-return 0; --> In C, it's used in the main function to indicate that the program has successfully executed.
+return 0; --> In C, it's used in the main function to indicate that the program has successfully executed.*/
 
 
 #include<stdio.h>
