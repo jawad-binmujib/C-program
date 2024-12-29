67a481ea8f02d335f8c6923a8112ba3d42adf056
jawad219 <jawadbinmujib219@gmail.com>
2024-12-30 00:56:56 +0600

    Mistakenly named the file input instead of output

---
+++ output.c
@@ -0,0 +1,17 @@
+// practical Examples
+
+#include<stdio.h>
+// Output Functions
+int main(){
+int age = 20;
+printf("You are %d years old\n", age); // Using printf
+
+char c = 'A';
+putchar(c); // using putchar
+
+puts("Hello, World!"); // using puts
+
+
+return 0;
+}
+// Happy Coding!!!!!!!!!!!!!!!
